6ee3a9c9da8e1f8b0a4d032e509079d7c90ab1d8
331uw13 <69628281+331uw13@users.noreply.github.com>
2021-05-19 17:44:43 +0300

    Add files via upload

--- demotool.c
+++ demotool.c
@@ -1,4 +1,3 @@
-#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <GL/glew.h>
@@ -15,84 +14,139 @@
 #define DT_PROGRAM_STATUS  1
 
 
+
 static GLFWwindow* window = NULL;
-static int window_width = 0;
-static int window_height = 0;
-static u8* audio_pos = 0;
-static u32 audio_length = 0;
+static u8* audio_pos     = 0;
+static u32 audio_length  = 0;
 static u32 default_vertex_shader = 0;
-static char** shader_include_src = NULL;
-static u64 shader_include_src_length = 0;
 
-static int  dt_check_status(u32 id, u32 flag);
-static void dt_delete_effects(struct shader_effect* effects, u32 count);
+static int dt_gflags = 0;
+static int dt_common_data[DT_NUM_COMMON_DATA];
+static u32 dt_current_shader = 0;
 
 
-u32 dt_create_program(char* fs_src) {
-	
+// TODO: try make this better.
+int dt_check_status(u32 id, u32 flag) {
+	int res = 0;
+	if(flag == DT_SHADER_STATUS || flag == DT_PROGRAM_STATUS) {
+		char* msg = NULL;
+		int msg_length = 0;
+
+		if(flag == DT_SHADER_STATUS) {
+			glGetShaderiv(id, GL_COMPILE_STATUS, &res);
+			glGetShaderiv(id, GL_INFO_LOG_LENGTH, &msg_length);
+		}
+		else if(flag == DT_PROGRAM_STATUS) {
+			glGetProgramiv(id, GL_LINK_STATUS, &res);
+			glGetProgramiv(id, GL_INFO_LOG_LENGTH, &msg_length);
+		}
+
+		if(msg_length > 0) {
+			msg = malloc(msg_length);
+			if(msg != NULL) {
+				if(flag == DT_SHADER_STATUS) {
+					glGetShaderInfoLog(id, msg_length, NULL, msg);
+				}
+				else if(flag == DT_PROGRAM_STATUS) {
+					glGetProgramInfoLog(id, msg_length, NULL, msg);
+				}
+				fprintf(stderr, "\033[33m==== SHADER INFO LOG ==================\033[0m\n%s\n", msg);
+			}
+			else {
+				fprintf(stderr, "ERROR: Failed to allocate %i bytes of memory!\n", msg_length);
+			}
+		}
+	}
+
+	return res;
+}
+
+
+void sdl_audio_callback(void* userdata, u8* stream, int bytes) {
+	if(audio_length == 0 || bytes <= 0 || stream == NULL) { return; }
+	bytes = ((u32)bytes > audio_length ? audio_length : (u32)bytes);
+
+	if(dt_gflags & DT_PAUSED) { 
+		memset(stream, 0, bytes);
+	}
+	else {
+		memmove(stream, audio_pos, bytes);
+		audio_length -= bytes;
+		audio_pos += bytes;
+	}
+}
+
+void glfw_key_callback(GLFWwindow* win, int key, int scancode, int action, int mods) {
+	if(action == GLFW_PRESS) {
+		switch(key) {
+
+			// Pause
+			case GLFW_KEY_P:
+			case GLFW_KEY_SPACE:
+				dt_set_flag(DT_PAUSED, !(dt_gflags & DT_PAUSED));
+				break;
+
+			// Quit
+			case GLFW_KEY_ESCAPE:
+			case GLFW_KEY_END:
+				glfwSetWindowShouldClose(win, 1);
+				break;
+		
+			default: break;
+		}
+	}
+}
+
+
+u32 dt_create_shader(char* fs_src) {
 	u32 fs = 0;
 	u32 program = 0;
 	
-	if(fs_src != NULL) {
-		
-		if(default_vertex_shader == 0) {
-			fprintf(stderr, "Failed to create program because of default vertex shader is not compiled!\n");
-			goto finish;
-		}
-
-		// Create fragment shader for program.
-
-		fs = glCreateShader(GL_FRAGMENT_SHADER);
-		if(fs == 0) {
-			fprintf(stderr, "Failed to create fragment shader!\n");
-			goto finish;
-		}
-
-		if(shader_include_src == NULL || shader_include_src_length == 0) {
-			glShaderSource(fs, 1, (const char**)&fs_src, NULL);
-		}
-		else {
-			const u64 origin_src_length = strlen(fs_src);
-			char* full_src = malloc(shader_include_src_length + origin_src_length + 1);
+	if(window != NULL && (dt_gflags & DT_INITIALIZED)) {
+		if(fs_src != NULL) {
 			
-			if(full_src != NULL) {
-				memmove(full_src, *shader_include_src, shader_include_src_length);
-				memmove(full_src + shader_include_src_length, fs_src, origin_src_length);
-
-				full_src[shader_include_src_length + origin_src_length] = '\0';
-				glShaderSource(fs, 1, (const char**)&full_src, NULL);
-			}
-			else {
-				fprintf(stderr, "Failed to allocate %li bytes of memory! errno: %i\n", 
-						shader_include_src_length + origin_src_length, errno);
+			if(default_vertex_shader == 0) {
+				fprintf(stderr, "Failed to create program because of default vertex shader is not compiled!\n");
 				goto finish;
 			}
+
+			// Create fragment shader for program.
+
+			fs = glCreateShader(GL_FRAGMENT_SHADER);
+			if(fs == 0) {
+				fprintf(stderr, "Failed to create fragment shader!\n");
+				goto finish;
+			}
+
+			glShaderSource(fs, 1, (const char**)&fs_src, NULL);
+			glCompileShader(fs);
+
+			if(!dt_check_status(fs, DT_SHADER_STATUS)) {
+				fprintf(stderr, "Failed to compile fragment shader!\n");
+				goto finish;
+			}
+
+			// Create the program.
+
+			program = glCreateProgram();
+			if(program == 0) {
+				fprintf(stderr, "Failed to create program!\n");
+				goto finish;
+			}
+
+			glAttachShader(program, default_vertex_shader);
+			glAttachShader(program, fs);
+			glLinkProgram(program);
+
+			if(!dt_check_status(program, DT_PROGRAM_STATUS)) {
+				fprintf(stderr, "Failed to link program!\n");
+				glDeleteProgram(program);
+			}
 		}
-
-		glCompileShader(fs);
-
-		if(!dt_check_status(fs, DT_SHADER_STATUS)) {
-			fprintf(stderr, "Failed to compile fragment shader!\n");
-			goto finish;
-		}
-
-		// Create the program.
-
-		program = glCreateProgram();
-		if(program == 0) {
-			fprintf(stderr, "Failed to create program!\n");
-			goto finish;
-		}
-
-		glAttachShader(program, default_vertex_shader);
-		glAttachShader(program, fs);
-
-		glLinkProgram(program);
-
-		if(!dt_check_status(program, DT_PROGRAM_STATUS)) {
-			fprintf(stderr, "Failed to link program!\n");
-			goto finish;
-		}
+	}
+	else {
+		fprintf(stderr, "ERROR: Cant create shader because not initialized! Exit.\n");
+		exit(-1);
 	}
 
 finish:
@@ -105,35 +159,38 @@ finish:
 	return program;
 }
 
-void dt_shader_include(char** src, u64 length) {
-	shader_include_src = src;
-	shader_include_src_length = (src == NULL) ? 0 : length;
-}
-
-void sdl_audio_callback(void* userdata, u8* stream, int bytes) {
-	if(audio_length == 0) { return; }
-	bytes = ((u32)bytes > audio_length ? audio_length : (u32)bytes);
-	SDL_memcpy(stream, audio_pos, bytes);
-	audio_pos += bytes;
-	audio_length -= bytes;
-}
-
-void glfw_key_callback(GLFWwindow* win, int key, int scancode, int action, int mods) {
-	if(key == GLFW_KEY_ESCAPE || key == GLFW_KEY_END && action == GLFW_PRESS) {
-		glfwSetWindowShouldClose(win, 1);
+void dt_delete_shader(u32 id) {
+	if(id > 1) {
+		glDeleteProgram(id);
+		if(id == dt_current_shader) {
+			dt_current_shader = 0;
+		}
 	}
-
 }
 
-void dt_initialize(char* title) {
+void dt_use_shader(u32 id) {
+	if(id > 1) {
+		glUseProgram(id);
+		dt_current_shader = id;
+	}
+}
+
+void dt_initialize(char* title, u32 flags) {
+	dt_gflags = flags;
+	dt_current_shader = 0;
 
 	if(!glfwInit()) {
-		fprintf(stderr, "Failed to initialize glfw!\n");
+		fprintf(stderr, "ERROR: Failed to initialize glfw!\n");
+		exit(-1);
 		return;
 	}
-
-	glfwWindowHint(GLFW_MAXIMIZED, 1);
-	glfwWindowHint(GLFW_FOCUS_ON_SHOW, 1);
+	
+	if(dt_gflags & DT_FULLSCREEN) {
+		glfwWindowHint(GLFW_MAXIMIZED, 1);
+	}
+	if(dt_gflags & DT_DISABLE_CURSOR) {
+		glfwWindowHint(GLFW_FOCUS_ON_SHOW, 1);
+	}
 	glfwWindowHint(GLFW_DECORATED, 0);
 	glfwWindowHint(GLFW_RESIZABLE, 0);
 	glfwWindowHint(GLFW_DOUBLEBUFFER, 1);
@@ -145,37 +202,63 @@ void dt_initialize(char* title) {
 	int monitor_y = 0;
 	int monitor_w = 0;
 	int monitor_h = 0;
+	glfwGetMonitorWorkarea(glfwGetPrimaryMonitor(), &monitor_x, &monitor_y, &monitor_w, &monitor_h);
+	
+	int win_req_w = (dt_gflags & DT_FULLSCREEN) ? monitor_w : dt_common_data[DT_WINDOW_W];
+	int win_req_h = (dt_gflags & DT_FULLSCREEN) ? monitor_h : dt_common_data[DT_WINDOW_H];
+	
+	if(win_req_w <= 0) {
+		win_req_w = DT_DEFAULT_WINDOW_WIDTH;
+	}
 
-	glfwGetMonitorWorkarea(glfwGetPrimaryMonitor(), 
-			&monitor_x, &monitor_y, &monitor_w, &monitor_h);
+	if(win_req_h <= 0) {
+		win_req_h = DT_DEFAULT_WINDOW_HEIGHT;
+	}
 
-	window = glfwCreateWindow(monitor_w, monitor_h, title, NULL, NULL);
+
+	window = glfwCreateWindow(win_req_w, win_req_h, title, NULL, NULL);
+
 	if(window == NULL) {
-		fprintf(stderr, "Failed to create window\n");
+		fprintf(stderr, "ERROR: Failed to create window (width: %i, height: %i)\n", win_req_w, win_req_h);
 		glfwTerminate();
+		exit(-1);
 		return;
 	}
 
-	glfwMakeContextCurrent(window);
-	glfwSwapInterval(1);
-	glfwGetWindowSize(window, &window_width, &window_height);
-	glfwSetKeyCallback(window, glfw_key_callback);
+	int* win_w = &dt_common_data[DT_WINDOW_W];
+	int* win_h = &dt_common_data[DT_WINDOW_H];
 
-	glViewport(0.0, 0.0, window_width, window_height);
+	glfwMakeContextCurrent(window);
+	glfwGetWindowSize(window, win_w, win_h);
+	
+	const int win_req_x = (dt_gflags & DT_FULLSCREEN) ? monitor_x : (monitor_w/2-*win_w/2);
+	const int win_req_y = (dt_gflags & DT_FULLSCREEN) ? monitor_y : (monitor_h/2-*win_h/2);
+	
+	glfwSetWindowPos(window, win_req_x, win_req_y);
+	glfwSetKeyCallback(window, glfw_key_callback);
+	glfwSwapInterval((dt_gflags & DT_USE_VSYNC));
+
+	glViewport(0.0, 0.0, *win_w, *win_h);
 	glClearColor(0.0, 0.0, 0.0, 1.0);
 
-	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
-	glfwSetWindowPos(window, monitor_x, monitor_y);
+	if(dt_gflags & DT_DISABLE_CURSOR) {
+		glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+	}
 	
 	if(glewInit() != GLEW_OK) {
-		fprintf(stderr, "Failed to initialize glew!\n");
+		fprintf(stderr, "ERROR: Failed to initialize glew!\n");
 		glfwDestroyWindow(window);
 		glfwTerminate();
+		exit(-1);
 		return;
 	}
 
 	if(SDL_Init(SDL_INIT_AUDIO) < 0) {
-		fprintf(stderr, "Failed to initialize SDL for audio! %s\n", SDL_GetError());
+		fprintf(stderr, "ERROR: Failed to initialize SDL for audio! %s\n"
+				"DT_NO_AUDIO is now set!\n", SDL_GetError());
+		dt_gflags |= DT_NO_AUDIO;
+		// Dont call exit() here... maybe user still wants to watch the demo?
+		// TODO: make sure user sees this error message and can decide if wanted to continue.
 	}
 
 	// Compile default vertex shader.
@@ -186,121 +269,126 @@ void dt_initialize(char* title) {
 			"#version 330\n"
 			"in vec2 pos;"
 			"void main() {"
-				"gl_Position = vec4(pos.x, pos.y, 0.0, 1.0);"
+				"gl_Position=vec4(pos.x,pos.y,0.0,1.0);"
 			"}";
 
 		glShaderSource(default_vertex_shader, 1, &vertex_source, NULL);
 		glCompileShader(default_vertex_shader);
 		
-		if(!dt_check_status(default_vertex_shader, DT_SHADER_STATUS)) {
-			fprintf(stderr, "Failed to compile default vertex shader!\n");
+		if(dt_check_status(default_vertex_shader, DT_SHADER_STATUS)) {
+			dt_gflags |= DT_INITIALIZED;
+		}
+		else {
+			fprintf(stderr, "ERROR: Failed to compile default vertex shader!\n");
+			dt_cleanup();
+			exit(-1);
 		}
 	}
 	else {
-		fprintf(stderr, "Failed to create default vertex shader!\n");
+		fprintf(stderr, "ERROR: Failed to create default vertex shader!\n");
 	}
 }
 
-void dt_play(struct shader_effect* effects, u32 effects_count, char* music_path) {
+void dt_play(char* music_path, void(*frame_update_callback)(double)) {
 	
-	if(effects == NULL || effects_count == 0) {
-		fprintf(stderr, "There are no effects to play...\n");
-		dt_delete_effects(effects, effects_count);
-		return;
+	if(frame_update_callback == NULL) {
+		fprintf(stderr, "WARNING: frame_update_callback is equal to NULL!\n");
 	}
-	
-	glDeleteShader(default_vertex_shader); // Not needed anymore.
 
-
-	// Setup audio.
-	
 	SDL_AudioSpec audio_spec;
-	SDL_AudioDeviceID audio_dev;
+	SDL_AudioDeviceID audio_dev = 0;
 	u8* audio_init_pos = NULL;
 	u32 audio_init_length = 0;
 
-	if(SDL_LoadWAV(music_path, &audio_spec, &audio_init_pos, &audio_init_length)) {
-		audio_pos = audio_init_pos;
-		audio_length = audio_init_length;
-		audio_spec.callback = sdl_audio_callback;
-		audio_dev = SDL_OpenAudioDevice(NULL, 0, &audio_spec, NULL, SDL_AUDIO_ALLOW_ANY_CHANGE);
-		if(audio_dev > 0) {
-			SDL_PauseAudioDevice(audio_dev, 0);
+	if(!(dt_gflags & DT_NO_AUDIO)) {
+		if(SDL_LoadWAV(music_path, &audio_spec, &audio_init_pos, &audio_init_length)) {
+			audio_pos = audio_init_pos;
+			audio_length = audio_init_length;
+			audio_spec.callback = sdl_audio_callback;
+
+			audio_dev = SDL_OpenAudioDevice(NULL, 0, &audio_spec, NULL, SDL_AUDIO_ALLOW_ANY_CHANGE);
+			
+			if(audio_dev > 0) {
+				SDL_PauseAudioDevice(audio_dev, 0);
+			}
+			else {
+				fprintf(stderr, "ERROR: Failed to open audio device! %s\n", SDL_GetError());
+				dt_gflags |= DT_NO_AUDIO;
+			}
 		}
 		else {
-			fprintf(stderr, "Failed to open audio device! %s\n", SDL_GetError());
+			fprintf(stderr, "ERROR: %s\n", SDL_GetError());
 		}
 	}
-	else {
-		fprintf(stderr, "Failed to load music file \"%s\"! %s\n", music_path, SDL_GetError());
-	}
-
-	struct shader_effect* eff = &effects[0];
-	u32 effect_index = 0;
-	glUseProgram(eff->program);
 
 	double start = glfwGetTime();
 	double elapsed = start;
-	double eff_start = start;
-	double eff_elapsed = start;
 	double dt = start;
-	const double min_dt = 60.0 / 1000.0;
+	double pause_time = 0.0;
+	const double min_dt = 60.0/1000.0;
+
 
 	while(!glfwWindowShouldClose(window) && audio_length > 0) {
 		if(dt < min_dt) {
 			// Previous frame finished early.
-			SDL_Delay((min_dt - dt) * 1000.0);
+			SDL_Delay((min_dt-dt)*1000.0);
 		}
 
-		const double time = glfwGetTime();
-		elapsed = time - start;
-		eff_elapsed = time - eff_start;
-		//printf("Elapsed: %f | Effect elapsed: %f\n", elapsed, eff_elapsed);
+		double time = glfwGetTime();
+	
+		if(!(dt_gflags & DT_PAUSED)) {
+			if(pause_time > 0.0) {
+				glfwSetTime(pause_time);
+				time = pause_time;
+				pause_time = 0.0;
+			}
+			elapsed = time - start;
+		}
+		else {
+			if(pause_time <= 0.0) {
+				pause_time = glfwGetTime();
+			}
+		}
 		
 		glfwPollEvents();
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		if(eff_elapsed >= eff->duration) {
-			if(effect_index < effects_count) {
-				eff_start = time;
-				effect_index++;
-				eff = &effects[effect_index];
-				
-				glUseProgram(eff->program);
-			}
+		if(frame_update_callback != NULL) {
+			frame_update_callback(elapsed);
 		}
 
-		glUniform2f(glGetUniformLocation(eff->program, "screen"), window_width, window_height);
-		glUniform1f(glGetUniformLocation(eff->program, "time"), elapsed);
-		glUniform1f(glGetUniformLocation(eff->program, "eff_time"), eff_elapsed);
-	
-		glBegin(GL_QUADS);
-		glVertex2f(-1.0, -1.0);
-		glVertex2f(-1.0,  1.0);
-		glVertex2f( 1.0,  1.0);
-		glVertex2f( 1.0, -1.0);
-		glVertex2f(-1.0, -1.0);
-		glEnd();
+		if(dt_current_shader > 0) {
+			glBegin(GL_QUADS);
+			glVertex2f(-1.0, -1.0);
+			glVertex2f(-1.0,  1.0);
+			glVertex2f( 1.0,  1.0);
+			glVertex2f( 1.0, -1.0);
+			glVertex2f(-1.0, -1.0);
+			glEnd();
+		}
 
 		glfwSwapBuffers(window);
 		dt = glfwGetTime() - time;
 	}
 
 	glUseProgram(0);
-
-
-	if(audio_dev > 0) {
-		SDL_CloseAudioDevice(audio_dev);
+	if(!(dt_gflags & DT_NO_AUDIO)) {
+		if(audio_dev > 0) {
+			SDL_CloseAudioDevice(audio_dev);
+		}
+		if(audio_init_pos != NULL) {
+			SDL_FreeWAV(audio_init_pos);
+		}
 	}
-	
-	if(audio_init_pos != NULL) {
-		SDL_FreeWAV(audio_init_pos);
-	}
 
-	dt_delete_effects(effects, effects_count);
+	dt_cleanup();
 }
 
-void dt_quit() {
+void dt_stop() {
+	glfwSetWindowShouldClose(window, 1);
+}
+
+void dt_cleanup() {
 	glfwDestroyWindow(window);
 	glfwTerminate();
 	SDL_Quit();
@@ -308,43 +396,61 @@ void dt_quit() {
 	puts("\033[32m Bye! <3\033[0m");
 }
 
-void dt_delete_effects(struct shader_effect* effects, u32 count) {
-	for(u32 i = 0; i < count; i++) {
-		glDeleteProgram(effects[i].program);
-	}
+u8 dt_get_flag(u32 flag) {
+	return dt_gflags & flag;
 }
 
-int dt_check_status(u32 id, u32 flag) {
-	int res = 0;
-	char* msg = NULL;
-	int msg_length = 0;
+void dt_set_flag(u32 flag, u8 b) {
+	b ? (dt_gflags |= flag) : (dt_gflags &= ~flag);
+}
 
-	if(flag == DT_SHADER_STATUS) {
-		glGetShaderiv(id, GL_COMPILE_STATUS, &res);
-		glGetShaderiv(id, GL_INFO_LOG_LENGTH, &msg_length);
+int dt_get(u32 i) {
+	int res = -1;
+	if(i < DT_NUM_COMMON_DATA) {
+		res = dt_common_data[i];
 	}
-	else if(flag == DT_PROGRAM_STATUS) {
-		glGetProgramiv(id, GL_LINK_STATUS, &res);
-		glGetProgramiv(id, GL_INFO_LOG_LENGTH, &msg_length);
-	}
-
-	if(msg_length > 0) {
-		msg = malloc(msg_length);
-		if(msg != NULL) {
-			if(flag == DT_SHADER_STATUS) {
-				glGetShaderInfoLog(id, msg_length, NULL, msg);
-			}
-			else if(flag == DT_PROGRAM_STATUS) {
-				glGetProgramInfoLog(id, msg_length, NULL, msg);
-			}
-
-			fprintf(stderr, "\033[33m==== SHADER INFO LOG ==================\033[0m\n%s\n", msg);
-		}
-		else {
-			fprintf(stderr, "Failed to allocate %i bytes of memory!\n", msg_length);
-		}
-	}
-
 	return res;
 }
+
+void dt_set(u32 i, int value) {
+	if(i < DT_NUM_COMMON_DATA) {
+		dt_common_data[i] = value;
+	}
+}
+
+
+void dt_shader_uniform_1f(char* name, double x) {
+	glUniform1f(glGetUniformLocation(dt_current_shader, name), x);
+}
+
+void dt_shader_uniform_2f(char* name, double x, double y) {
+	glUniform2f(glGetUniformLocation(dt_current_shader, name), x, y);
+}
+
+void dt_shader_uniform_3f(char* name, double x, double y, double z) {
+	glUniform3f(glGetUniformLocation(dt_current_shader, name), x, y, z);
+}
+
+void dt_shader_uniform_4f(char* name, double x, double y, double z, double w) {
+	glUniform4f(glGetUniformLocation(dt_current_shader, name), x, y, z, w);
+}
+
+
+void dt_shader_uniform_1i(char* name, int x) {
+	glUniform1i(glGetUniformLocation(dt_current_shader, name), x);
+}
+
+void dt_shader_uniform_2i(char* name, int x, int y) {
+	glUniform2i(glGetUniformLocation(dt_current_shader, name), x, y);
+}
+
+void dt_shader_uniform_3i(char* name, int x, int y, int z) {
+	glUniform3i(glGetUniformLocation(dt_current_shader, name), x, y, z);
+}
+
+void dt_shader_uniform_4i(char* name, int x, int y, int z, int w) {
+	glUniform4i(glGetUniformLocation(dt_current_shader, name), x, y, z, w);
+}
+
+
 
--- demotool.h
+++ demotool.h
@@ -7,19 +7,65 @@ typedef unsigned int       u32;
 typedef long unsigned int  u64;
 
 
-struct shader_effect {
-	u32     program;
-	double  duration;
+// Global flags can be set or unset with 'dt_set_flag()'
+// and check if they are set with 'dt_get_flag()'
+// NOTE: some flags may not affect anything if changed after calling 'dt_play()' or 'dt_initialize()'
+#define DT_INITIALIZED      0x1
+#define DT_NO_AUDIO         0x2
+#define DT_PAUSED           0x4
+#define DT_FULLSCREEN       0x8
+#define DT_DISABLE_CURSOR   0x10
+#define DT_USE_VSYNC        0x20
+
+// Some default values for things..
+#define DT_DEFAULT_WINDOW_WIDTH      1000
+#define DT_DEFAULT_WINDOW_HEIGHT     800
+
+#define DT_NUM_COMMON_DATA 2  // Used to check if user calls 'dt_set()' or 'dt_get()'
+// to check that parameter 'i' is in range so it wont crash if its too big by mistake.
+
+// Can be used with 'dt_set()' and 'dt_get()' for 'u32 i'
+#define DT_WINDOW_W  0  // Window width.
+#define DT_WINDOW_H  1  // Window height.
+
+
+struct dt_timep {
+	int     id;       // Position in array.
+	u8      playing;  // Is it active right now?
+	u8      played;   // Is it now already played?
 };
 
-void dt_initialize(char* title);
-void dt_quit();
+void dt_initialize(char* title, u32 flags);
+void dt_play(char* music_path, void(*frame_update_callback)(double));
+void dt_stop();
+void dt_cleanup();
+double dt_get_time();
+//double dt_random(double min, double max);
 
-u32  dt_create_program(char* fs_src);
-void dt_shader_include(char** src, u64 length);
+u32  dt_create_shader(char* fs_src);
+void dt_delete_shader(u32 id);
+void dt_use_shader(u32 id);
+/*
+void dt_alloc_timepoints(int count);
+void dt_add_timepoint(double time, int id);
+void dt_delete_timepoint(int id);
+void dt_get_timepoint(struct dt_timep* out);
+*/
+u8   dt_get_flag(u32 flag);
+void dt_set_flag(u32 flag, u8 b);
+
+int  dt_get(u32 i);
+void dt_set(u32 i, int value);
 
 
-void dt_play(struct shader_effect* effects, u32 effects_count, char* music_path);
-
+// These affect the current shader that was set with 'dt_use_shader()'
+void dt_shader_uniform_1f(char* name, double x);
+void dt_shader_uniform_2f(char* name, double x, double y);
+void dt_shader_uniform_3f(char* name, double x, double y, double z);
+void dt_shader_uniform_4f(char* name, double x, double y, double z, double w);
+void dt_shader_uniform_1i(char* name, int x);
+void dt_shader_uniform_2i(char* name, int x, int y);
+void dt_shader_uniform_3i(char* name, int x, int y, int z);
+void dt_shader_uniform_4i(char* name, int x, int y, int z, int w);
 
 #endif
